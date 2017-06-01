69b20e67afaa9d911ae9ccc34442427ac3b3e8aa
Jimmy Börjesson <alcesleo@gmail.com>
2017-06-01 16:18:32 +0200

    Support Gemfile and add failure case to install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,19 @@
 
 INSTALL_PATH=$(pwd)
 
-echo "Installing alcesleo/rubocop-config into $(pwd)"
+echo "Installing alcesleo/rubocop-config into $INSTALL_PATH"
 
 # Install the dependency in the gemspec or Gemfile
 if [ -f *.gemspec ]; then
-    sed -i '' 's/^end/  spec.add_development_dependency "rubocop", "~> 0.48"\'$'\nend/g' *.gemspec
+    echo "Installing development dependency in gemspec"
+
+    # `-i ''` replaces in file without backup
+    # `\'$'\n` creates the newline
+    sed -i '' 's/^end/  spec.add_development_dependency "rubocop", "~> 0.48"\'$'\nend/' *.gemspec
+elif [ -f Gemfile ]; then
+    echo "Installing development dependency in Gemfile"
+    echo 'gem "rubocop", "~> 0.48"' >> Gemfile
+else
+    echo "No gemspec or Gemfile found in $INSTALL_PATH"
+    exit 1
 fi
