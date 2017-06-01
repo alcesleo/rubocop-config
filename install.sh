41aaf466475595a5a9f8e64980d935d38b231026
Jimmy Börjesson <alcesleo@gmail.com>
2017-06-01 16:24:29 +0200

    Support .gitignore in install.sh

--- install.sh
+++ install.sh
@@ -22,3 +22,6 @@ fi
 # Add the inherit statement to .rubocop.yml
 echo "- Adding inherit_from in .rubocop.yml "
 echo -e "inherit_from:\n  - https://raw.githubusercontent.com/alcesleo/rubocop-config/master/.rubocop.yml\n$(cat .rubocop.yml 2>/dev/null)" > .rubocop.yml
+
+# Add it to .gitignore
+echo ".rubocop-https---raw-githubusercontent-com-alcesleo-rubocop-config-master--rubocop-yml" >> .gitignore
